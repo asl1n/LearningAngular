17a30428af28073821c099accbe95f0d8ff0acbe
Aslin <aslinkarmacharya@gmail.com>
2025-03-31 11:00:32 +0545

    progress

--- src/app/mero-form/mero-form.component.ts
+++ src/app/mero-form/mero-form.component.ts
@@ -4,19 +4,21 @@ import { Hero } from './hero';
 @Component({
   selector: 'app-mero-form',
   templateUrl: './mero-form.component.html',
-  styleUrls: ['./mero-form.component.scss']
+  styleUrls: ['./mero-form.component.css']
+
 })
-
 export class MeroFormComponent {
-    powers = ['Really smart', 'idiot', 'super hot', 'Weather Changer'];
-    model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
-    submitted = false;
-    onSubmit(){
-      this.submitted = true;
-    }
 
-    myHero =  new Hero(42, 'SkyDog',
-      'Fetch any object at any distance',
-      'Leslie Rollover');
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
 
+  model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
 }
