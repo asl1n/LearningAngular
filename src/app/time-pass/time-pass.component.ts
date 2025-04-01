87f6eaabe9bc1f572eea700867891ef812956d15
Aslin <aslinkarmacharya@gmail.com>
2025-04-01 11:10:54 +0545

    redoing different approach

--- src/app/time-pass/time-pass.component.ts
+++ src/app/time-pass/time-pass.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-time-pass',
@@ -6,11 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./time-pass.component.scss'],
 })
 export class TimePassComponent {
-  name = '';
-  message = '';
+  meroForm: FormGroup;
+  choices= ['Complaining','Checking','Need Support'];
+
+  constructor(private fb: FormBuilder) {
+    this.meroForm = fb.group({
+      name: ['', Validators.required],
+      message: ['', Validators.required],
+      choice: ['', Validators.required]
+    });
+  }
 
   submit() {
-    if (this.name === '' && this.message === '') {
+    if (this.meroForm.invalid) {
       alert('Please enter a name and message');
     } else {
       console.log('done');
